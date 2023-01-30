8a0946e45926c7901838c510f3044602d93c7031
314esther <55110002+314esther@users.noreply.github.com>
2023-01-30 16:09:24 -0500

    Update main.py

--- gpt_index/main.py
+++ gpt_index/main.py
@@ -5,9 +5,12 @@ documents = SimpleDirectoryReader('gpt_index/data').load_data()
 
 index = GPTSimpleVectorIndex(documents)
 
+st.warning('DISCLAIMER: The answer to the question you have asked is provided by GPT3. GPT3 is a large language model and has been trained on material written by Henry Lieberman and Christopher Fry, however the responses here are not authored by Lieberman or Fry. The responses are also stocastic, meaning that if you ask the same question twice, you may recieve different answers to the same question. This is not an authoritative source!', icon="⚠️")
+
 question = st.text_input("Ask me a question")
 
 if question:
   response = index.query(question)
 
   st.write(response)
+  
